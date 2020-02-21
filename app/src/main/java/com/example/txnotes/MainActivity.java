6f9955df69a7ee96bb688992d5d55845aceba98e
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2020-02-21 21:03:30 +0300

    Add button and corresponding function (still empty)

--- app/src/main/java/com/example/txnotes/MainActivity.java
+++ app/src/main/java/com/example/txnotes/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.txnotes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Adds a new note and shows NewNoteActivity
+    public void addNewNote(View view) {
+        // Do something in response to button click
     }
 }
