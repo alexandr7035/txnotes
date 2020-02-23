72a01a4d2a9bdcb921d0ad688ace75fb0754c97b
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2020-02-23 17:11:48 +0300

    Add a layout for notes with empty button

--- app/src/main/java/com/example/txnotes/MainActivity.java
+++ app/src/main/java/com/example/txnotes/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.example.txnotes.db.TXDatabase;
 
@@ -14,6 +16,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout notes_layout = findViewById(R.id.notesLayout);
+        notes_layout.addView(new Button(getApplicationContext()));
+
     }
 
     // Adds a new note and shows NewNoteActivity
