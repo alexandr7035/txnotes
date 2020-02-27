9bd67d90a5787550cf0368bfce5eea6b75fa57af
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2020-02-27 22:14:38 +0300

    Remove note title

--- app/src/main/java/com/example/txnotes/CreateNewNoteActivity.java
+++ app/src/main/java/com/example/txnotes/CreateNewNoteActivity.java
@@ -1,7 +1,5 @@
 package com.example.txnotes;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
@@ -9,6 +7,8 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 
 public class CreateNewNoteActivity extends AppCompatActivity {
@@ -24,7 +24,6 @@ public class CreateNewNoteActivity extends AppCompatActivity {
         setTitle(title_string);
 
         // Elements' objects to operate
-        final EditText note_title = findViewById(R.id.noteTitle);
         final EditText note_text = findViewById(R.id.noteText);
         final Button save_btn = findViewById(R.id.saveNote);
 
@@ -69,10 +68,6 @@ public class CreateNewNoteActivity extends AppCompatActivity {
     public void btnAddNote(View view) {
 
         EditText text;
-
-        // Get note title from 'noteTitle' element
-        text = findViewById(R.id.noteTitle);
-        String note_title = text.getText().toString();
 
         // Get note text from 'noteText' element
         text = findViewById(R.id.noteText);
