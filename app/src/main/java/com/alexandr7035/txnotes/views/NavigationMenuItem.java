90257cdee84e18064af2271e3d46a7ac87429fd2
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2020-04-05 20:38:54 +0300

    Image is shown in NavigationMenuItem

--- app/src/main/java/com/alexandr7035/txnotes/views/NavigationMenuItem.java
+++ app/src/main/java/com/alexandr7035/txnotes/views/NavigationMenuItem.java
@@ -2,7 +2,9 @@ package com.alexandr7035.txnotes.views;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -11,6 +13,7 @@ import com.alexandr7035.txnotes.R;
 public class NavigationMenuItem extends LinearLayout {
 
     private TextView textView;
+    private ImageView imageView;
 
     public NavigationMenuItem(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -18,14 +21,23 @@ public class NavigationMenuItem extends LinearLayout {
 
         // Child views
         textView = findViewById(R.id.text);
+        imageView = findViewById(R.id.image);
 
         // Attribute set
         TypedArray arr = context.obtainStyledAttributes(attrs, R.styleable.NavigationMenuItem);
 
+        // Text
         String item_text = arr.getString(R.styleable.NavigationMenuItem_item_text);
 
         if (item_text != null) {
             textView.setText(item_text);
+        }
+
+        // Image
+        Drawable item_image = arr.getDrawable(R.styleable.NavigationMenuItem_item_icon);
+
+        if (item_image != null) {
+            imageView.setImageDrawable(item_image);
         }
 
         arr.recycle();
