2fcf1aac0d76c5b6fd744d864b83c1c2be5eae49
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2020-05-07 00:24:05 +0300

    Prepare for using livedata
    Add MainViewModel class
    Update NotesDatabase class

---
+++ app/src/main/java/com/alexandr7035/txnotes/viewmodel/MainViewModel.java
@@ -0,0 +1,72 @@
+package com.alexandr7035.txnotes.viewmodel;
+
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.alexandr7035.txnotes.db.NoteEntity;
+import com.alexandr7035.txnotes.db.NotesDao;
+import com.alexandr7035.txnotes.db.NotesDatabase;
+
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class MainViewModel extends AndroidViewModel {
+
+    private LiveData<List<NoteEntity>> notesList;
+    private NotesRepository repository;
+
+    public MainViewModel(Application application){
+        super(application);
+
+        repository = new NotesRepository(application);
+    }
+
+    public LiveData<List<NoteEntity>> getNotesList()  {
+        return notesList;
+    }
+
+    public void addNote(NoteEntity note) {
+        repository.addNoteToDb(note);
+    }
+
+    public void removeNote(NoteEntity note) {
+        repository.removeNoteFromDb(note);
+    }
+}
+
+
+// May be moved to separate file if needed
+class NotesRepository {
+
+    private NotesDao dao;
+    private Executor executor;
+
+    NotesRepository(Application application) {
+        dao = NotesDatabase.getDatabase(application).getNotesDao();
+
+        // To run background tasks
+        executor = Executors.newSingleThreadExecutor();
+    }
+
+    void addNoteToDb(final NoteEntity note) {
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                dao.addNote(note);
+            }
+        });
+    }
+
+    void removeNoteFromDb(final NoteEntity note) {
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                dao.deleteNote(note);
+            }
+        });
+    }
+
+}
