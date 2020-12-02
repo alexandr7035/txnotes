f5777667f1259c87223f75002b4a8412ae1962e4
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2020-12-02 23:31:52 +0300

    Add livedata to note activity

--- app/src/main/java/com/alexandr7035/txnotes/activities/NoteActivity.java
+++ app/src/main/java/com/alexandr7035/txnotes/activities/NoteActivity.java
@@ -3,16 +3,23 @@ package com.alexandr7035.txnotes.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 
 import com.alexandr7035.txnotes.R;
 
 public class NoteActivity extends AppCompatActivity {
 
-    // Views
-    private Toolbar toolbar;
+    private EditText noteTextView;
+    private MutableLiveData<String> activityStateLiveData;
+
+    private TextView toolbarTitle;
 
     private int note_id;
 
@@ -21,8 +28,11 @@ public class NoteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
 
+        // Views
+        final Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbarTitle = findViewById(R.id.toolbarTitle);
+        noteTextView = findViewById(R.id.noteTextView);
 
-        toolbar = findViewById(R.id.toolbar);
         // Close activity on navigation btn click
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,17 +41,47 @@ public class NoteActivity extends AppCompatActivity {
             }
         });
 
+        // Init state LiveData
+        activityStateLiveData = new MutableLiveData<String>();
+
+
+        activityStateLiveData.observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String activityState) {
+
+                if (activityState != null) {
+                    if (activityState.equals("STATE_CREATING")) {
+                        toolbarTitle.setText(getString(R.string.activity_create_note_title));
+                        noteTextView.setEnabled(true);
+                    }
+                    else if (activityState.equals("STATE_SHOWING")) {
+                        toolbarTitle.setText(getString(R.string.activity_show_note_title));
+                        noteTextView.setEnabled(false);
+                    }
+
+                }
+
+            }
+        });
+
 
         // Get note_id
+        // 0 means a new note is creating
         Intent intent = getIntent();
         note_id = intent.getIntExtra("passed_note_id", 0);
 
+        //
         if (note_id == 0) {
-
+            activityStateLiveData.postValue("STATE_CREATING");
+        }
+        else {
+            activityStateLiveData.postValue("STATE_SHOWING");
         }
 
     }
 
-
-
 }
+
+
+
+
