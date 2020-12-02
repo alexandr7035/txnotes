16d4288dc40de1ce4ea702237d393e78860c9516
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2020-12-03 00:47:49 +0300

    Toolbar btn changes activity state

--- app/src/main/java/com/alexandr7035/txnotes/activities/NoteActivity.java
+++ app/src/main/java/com/alexandr7035/txnotes/activities/NoteActivity.java
@@ -2,6 +2,9 @@ package com.alexandr7035.txnotes.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -14,12 +17,17 @@ import androidx.lifecycle.Observer;
 
 import com.alexandr7035.txnotes.R;
 
-public class NoteActivity extends AppCompatActivity {
+public class NoteActivity extends AppCompatActivity
+                          implements Toolbar.OnMenuItemClickListener {
+
+    private final String LOG_TAG = "DEBUG_TXNOTES";
 
     private EditText noteTextView;
     private MutableLiveData<String> activityStateLiveData;
 
     private TextView toolbarTitle;
+
+    private Menu menu;
 
     private int note_id;
 
@@ -33,6 +41,7 @@ public class NoteActivity extends AppCompatActivity {
         toolbarTitle = findViewById(R.id.toolbarTitle);
         noteTextView = findViewById(R.id.noteTextView);
 
+
         // Close activity on navigation btn click
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +49,10 @@ public class NoteActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        toolbar.inflateMenu(R.menu.menu_note_activity_toolbar);
+        toolbar.setOnMenuItemClickListener(this);
+        invalidateOptionsMenu();
 
         // Init state LiveData
         activityStateLiveData = new MutableLiveData<String>();
@@ -54,9 +67,16 @@ public class NoteActivity extends AppCompatActivity {
                         toolbarTitle.setText(getString(R.string.activity_create_note_title));
                         noteTextView.setEnabled(true);
                     }
+
                     else if (activityState.equals("STATE_SHOWING")) {
                         toolbarTitle.setText(getString(R.string.activity_show_note_title));
                         noteTextView.setEnabled(false);
+
+                    }
+
+                    else if (activityState.equals("STATE_EDITING")) {
+                        toolbarTitle.setText(getString(R.string.activity_edit_note_title));
+                        noteTextView.setEnabled(true);
                     }
 
                 }
@@ -79,6 +99,39 @@ public class NoteActivity extends AppCompatActivity {
         }
 
     }
+
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+
+
+        // FIXME
+        // Simplify later
+        if (item.getItemId() == R.id.toolbar_btn_change_activity_state) {
+
+            Log.d(LOG_TAG, "btn clicked");
+
+            if (activityStateLiveData.getValue() != null) {
+                Log.d(LOG_TAG, "state not null");
+                if (activityStateLiveData.getValue().equals("STATE_CREATING")) {
+                    activityStateLiveData.postValue("STATE_SHOWING");
+                }
+
+                else if (activityStateLiveData.getValue().equals("STATE_SHOWING")) {
+                    activityStateLiveData.postValue("STATE_EDITING");
+                }
+
+                else if (activityStateLiveData.getValue().equals("STATE_EDITING")) {
+                    activityStateLiveData.postValue("STATE_SHOWING");
+                }
+
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
 
 }
 
