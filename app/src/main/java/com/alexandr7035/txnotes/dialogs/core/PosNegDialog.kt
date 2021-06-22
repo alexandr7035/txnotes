dc94629114f0c52a2162a35eabf8a770472040b6
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2021-06-22 23:32:03 +0300

    Remove binding buttons in PosNegDialog.kt

--- app/src/main/java/com/alexandr7035/txnotes/dialogs/core/PosNegDialog.kt
+++ app/src/main/java/com/alexandr7035/txnotes/dialogs/core/PosNegDialog.kt
@@ -14,18 +14,6 @@ abstract class PosNegDialog: CustomDialog(), View.OnClickListener {
     abstract var btnNegative: TextView
     abstract var btnPositive: TextView
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        val dialogView: View = inflater.inflate(R.layout.dialog_export_notes, container, false)
-
-        dialog?.window?.setBackgroundDrawable(resources.getDrawable(R.drawable.background_dialog))
-        isCancelable = false
-
-        btnNegative.setOnClickListener(this)
-        btnPositive.setOnClickListener(this)
-
-        return dialogView
-    }
-
     override fun onClick(v: View) {
         if (v.id == btnNegative.id) {
             actionHandler.onNegativeClick()
