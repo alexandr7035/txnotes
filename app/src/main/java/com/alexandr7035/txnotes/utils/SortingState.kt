4576a4a54e06887d71bebb96856f6b5135a71de8
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2022-01-08 14:41:35 +0300

    Recreate project for 4.0

--- app/src/main/java/com/alexandr7035/txnotes/utils/SortingState.kt
+++
@@ -1,9 +0,0 @@
-package com.alexandr7035.txnotes.utils
-
-class SortingState {
-    companion object {
-        val SORT_BY_MDATE_DESC = "SORT_BY_MDATE_DESC"
-        val SORT_BY_MDATE = "SORT_BY_MDATE"
-        val SORT_BY_TEXT = "SORT_BY_TEXT"
-    }
-}
