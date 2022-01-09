4b34eca480cb4dd5465f68f2ce7bdf42a49400d4
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2022-01-09 17:21:29 +0300

    Disable dark mode in activity

--- app/src/main/java/by/alexandr7035/txnotes/presentation/MainActivity.kt
+++ app/src/main/java/by/alexandr7035/txnotes/presentation/MainActivity.kt
@@ -2,6 +2,7 @@ package by.alexandr7035.txnotes.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import by.alexandr7035.txnotes.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,5 +11,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // TODO dark mode
+        // Temporarily disable here
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 }
