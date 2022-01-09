c44a50057046cb30565b94069a10a518caa26603
Alexandr Alexeenko <alexandr7035.adm@gmail.com>
2022-01-09 14:45:01 +0300

    Domain logics for crete note use case

--- domain/src/main/java/by/alexandr7035/domain/usecase/CreateNoteUseCase.kt
+++ domain/src/main/java/by/alexandr7035/domain/usecase/CreateNoteUseCase.kt
@@ -1,12 +1,26 @@
 package by.alexandr7035.domain.usecase
 
 import by.alexandr7035.domain.model.CreateNoteModel
-import by.alexandr7035.domain.model.Note
 import by.alexandr7035.domain.repository.NotesRepository
+import by.alexandr7035.domain.extentions.getSubString
 import javax.inject.Inject
 
 class CreateNoteUseCase @Inject constructor(private val repository: NotesRepository) {
     suspend fun execute(note: CreateNoteModel) {
-        repository.saveNote(note)
+
+        val title = if (note.title.trim().isBlank()) {
+            note.text.getSubString(DEFAULT_TITLE_LENGTH)
+        }
+        else {
+            note.title
+        }
+
+        val text = note.text.trim()
+
+        repository.saveNote(CreateNoteModel(title = title, text = text))
+    }
+
+    companion object {
+        private const val DEFAULT_TITLE_LENGTH = 100
     }
 }
